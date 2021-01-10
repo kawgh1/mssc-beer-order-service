c784ec8d2a1a5e5d2976ee32ebff5f79da3c0a70
thrwwy216 <thrwwy216@tutanota.com>
2021-01-10 11:30:22 -0600

    added validation result listener

---
+++ src/main/java/com/kwgdev/beer/order/service/services/listeners/ValidationResultListener.java
@@ -0,0 +1,26 @@
+package com.kwgdev.beer.order.service.services.listeners;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.jms.annotation.JmsListener;
+
+/**
+ * created by kw on 1/10/2021 @ 11:25 AM
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ValidationResultListener {
+
+    private final BeerOrderManager beerOrderManager;
+
+    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE)
+    public void listen(ValidateOrderResult result){
+        final UUID beerOrderId = result.getOrderId();
+
+        log.debug("Validation Result for Order Id: " + beerOrderId);
+
+        beerOrderManager.processValidationResult(beerOrderId, result.getIsValid());
+    }
+}
