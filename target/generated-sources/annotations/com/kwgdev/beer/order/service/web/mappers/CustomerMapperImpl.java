878e40be42089c1a0269e3947fe15a0442b649bb
thrwwy216 <thrwwy216@tutanota.com>
2021-01-28 22:21:16 -0600

    added local docker build, updated POM file for docker

--- target/generated-sources/annotations/com/kwgdev/beer/order/service/web/mappers/CustomerMapperImpl.java
+++ target/generated-sources/annotations/com/kwgdev/beer/order/service/web/mappers/CustomerMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-27T23:08:48-0600",
+    date = "2021-01-28T22:11:56-0600",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
 )
 @Component
